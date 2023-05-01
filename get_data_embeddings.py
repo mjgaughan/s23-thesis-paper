408fe8676997d1c7a0626c1124c3cf65490a4acf
mjgaughan <mgaughan@proton.me>
2023-04-30 22:18:42 -0700

    about to test embeddings grab

--- get_data_embeddings.py
+++ get_data_embeddings.py
@@ -1,25 +1,42 @@
 import os
 import openai
-import time
+import pandas as pd
+import tiktoken
+import tqdm
 
 openai.api_key = os.getenv("S23PARAML")
 #https://platform.openai.com/docs/guides/embeddings/use-cases
 
-# list models
-models = openai.Model.list()
+# embedding model parameters (https://github.com/openai/openai-cookbook/blob/main/examples/Obtain_dataset.ipynb)
+embedding_model = "text-embedding-ada-002"
+embedding_encoding = "cl100k_base"  # this the encoding for text-embedding-ada-002
+max_tokens = 8000  # the maximum for text-embedding-ada-002 is 8191
 
-#define the question for the model
-premise = "In Linux, there is a function parameter with the title 'foobar'." 
-context = "foobar is a pointer to an integer array."
-question = "foobar is only read from in its parent function, true or false?"
-promptstring = premise + question
-# create a completion
-completion = openai.Completion.create(model="ada", prompt=promptstring)
-#is it a buffering thing?
-time.sleep(15)
+input_datapath = "../../nobu-data/s23-thesis-paper/labeled_body_shuffled_ds_0.csv" 
+df = pd.read_csv(input_datapath, index_col=0)
+print(df.head())
+target_params = []
 
-# print the completion
-print("——————————————THE MODEL RESPONDS:")
-for choice in completion.choices:
-    print(choice.text)
-#print(completion.choices.text)
+#cleaning to isolate the param entry as its own (should work)
+with tqdm.tqdm(total=df.shape[0]) as pbar:
+    for index, row in df.iterrows():
+        pbar.update(1)
+        for i in range(10):
+            current_param_entry = row["a" + str(i)]
+            if current_param_entry != "ignore" and current_param_entry != "u":
+                target_params.append(current_param_entry)
+#this should now contain target_param as the parameter we're looking at
+df['target_param'] = target_params
+print(df.head())
+
+embeddings_df = pd.DataFrame()
+embeddings_df['param'] = df['target_param']
+
+#using OpenAI tokeniser because this is how they'd like us to use it
+#https://github.com/openai/tiktoken
+'''
+encoding = tiktoken.get_encoding(embedding_encoding)
+# This may take a few minutes
+embeddings_df["embedding"] = df['target_param'].apply(lambda x: get_embedding(x, engine=embedding_model))
+df.to_csv("nobu-data/s23-thesis-paper/fine_food_reviews_with_embeddings_1k.csv")
+'''
